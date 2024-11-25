aa9eb32f8c87355b0889fc579515845d950ca98e
Jorge Gomez <jtgomezmir89@gmail.com>
2024-11-25 13:11:11 -0300

    Python

--- Python/LK.py
+++ Python/LK.py
@@ -5,37 +5,52 @@ import numpy as np
 from sympy import *
 from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
+import math
 
-rho = 10
+
+rho = 0.01
 Vc = 0.5
 Qo = 0.1
 Co = Qo/Vc
 tau = rho*Co
-k = 3*np.sqrt(3)/2
+k = 3*np.sqrt(3)/2#3*np.sqrt(3)/2
 
 print("k: ", k)
 print("tau: ", tau)
 
-def differential_equation(t, Qn, Vn):
+def df(t, Qn, Vn):
     dQn_dt = (Vn + k * Qn - k * Qn**3)/tau
     return dQn_dt
 
-def differential_equation_II(t, Qn, Vn):
+def df_II(t, Qn, Vn):
     dQn_dt = (Vn + k * Qn - k * Qn**3)/(tau + tau*Qn**2)
     return dQn_dt
 
-def differential_equation_III(t, Qn, Vn):
+def df_III(t, Qn, Vn):
+    #dQn_dt = (Vn + k*(Qn - Qn**3 - (3/5)*Qn**5 - (3/7)*Qn**7))/(tau*(1 + Qn**2 + Qn**4 + Qn**6))
     dQn_dt = (Vn + k*(Qn - Qn**3 - (3/5)*Qn**5 - (3/7)*Qn**7))/(tau*(1 + Qn**2 + Qn**4 + Qn**6))
     return dQn_dt
 
-def differential_equation_IV(t, Qn, Vn):
-    arctanh_Qn = np.arctanh(Qn)  # Calculate once and reuse
+def df_IV(t, Qn, Vn):
+    Qn = np.clip(Qn, -0.99999999999, 0.99999999999)
+    arctanh_Qn = np.arctanh(Qn) 
     dQn_dt = ((1 - Qn**2) / tau) * (Vn - 3 * k * arctanh_Qn + 4 * k * Qn)
     return dQn_dt
 
+def df_V(t, Qn, Vn):
+    Qn = np.clip(Qn, -0.99999999999, 0.99999999999)
+    arctanh_Qn = np.arctanh(Qn) 
+    k1= 2.62    #4.03,2.49,3.91,2.6
+    k2=1 #0.77*k1-1.02#2.1,0.9,2,1
+    print("k1:", k1)
+    print("k2:", k2)
+    dQn_dt = ((1 - Qn**2) / tau) * (Vn +k1* Qn - k2*arctanh_Qn) #sale de la implementación en spice
+    #dQn_dt = ((1 - Qn**2) / tau) * (k1*Vn +k3*k1*Qn -arctanh_Qn)
+    return dQn_dt
 
-Vfe_up = np.linspace(-2, 2, 50)
-Vfe_down = np.linspace(2, -2, 50)
+
+Vfe_up = np.linspace(-2, 2, 500)
+Vfe_down = np.linspace(2, -2, 500)
 Vfe_values = np.concatenate((Vfe_up, Vfe_down))
 Vn_values = Vfe_values
 
@@ -44,30 +59,47 @@ Qn_final_values = []
 Qn_final_values_II = []
 Qn_final_values_III = []
 Qn_final_values_IV = []
+Qn_final_values_V = []
 
 t_span = (0, 3)
-Qn_initial = [-1.0]
+Qn_initial = [-1]
+Qn_initial_II = [-1]
+Qn_initial_III = [-1]
+Qn_initial_IV= [-1]
+Qn_initial_V= [-1]
 
 
 # Solve the differential equation for each value of Vfe
 for Vn in Vn_values:
-    solution = solve_ivp(differential_equation, t_span, Qn_initial, args=(Vn,), t_eval=[t_span[1]])
-    Qn_final_values.append(solution.y[0][-1])
-    Qn_initial = [solution.y[0][-1]]
+    #solution = solve_ivp(df, t_span, Qn_initial, args=(Vn,), t_eval=[t_span[1]])
+    #Qn_final_values.append(solution.y[0][-1])
+    #Qn_initial = [solution.y[0][-1]]
 
-    solution_II = solve_ivp(differential_equation_II, t_span, Qn_initial, args=(Vn,), t_eval=[t_span[1]])
-    Qn_final_values_II.append(solution_II.y[0][-1])
-    Qn_initial_II = [solution_II.y[0][-1]]
+    #solution_II = solve_ivp(df_II, t_span, Qn_initial_II, args=(Vn,), t_eval=[t_span[1]])
+    #Qn_final_values_II.append(solution_II.y[0][-1])
+    #Qn_initial_II = [solution_II.y[0][-1]]
 
-    solution_III = solve_ivp(differential_equation_III, t_span, Qn_initial, args=(Vn,), t_eval=[t_span[1]])
-    Qn_final_values_III.append(solution_III.y[0][-1])
-    Qn_initial_III = [solution_III.y[0][-1]]
+    #solution_III = solve_ivp(df_III, t_span, Qn_initial_III, args=(Vn,), t_eval=[t_span[1]])
+    #Qn_final_values_III.append(solution_III.y[0][-1])
+    #Qn_initial_III = [solution_III.y[0][-1]]
 
-    #solution_IV = solve_ivp(differential_equation_IV, t_span, Qn_initial, args=(Vn,), t_eval=[t_span[1]])
+    #solution_IV = solve_ivp(df_IV, t_span, Qn_initial_IV, args=(Vn,), t_eval=[t_span[1]]) 
+                            #max_step=0.001, 
+                            #rtol=1e-7, 
+                            #atol=1e-9,
+                            #method='DOP853')       
     #Qn_final_values_IV.append(solution_IV.y[0][-1])
     #Qn_initial_IV = [solution_IV.y[0][-1]]
 
-    print(Vn)
+    solution_V = solve_ivp(df_V, t_span, Qn_initial_V, args=(Vn,), t_eval=[t_span[1]],
+                            max_step=0.01, 
+                            rtol=1e-6, 
+                            atol=1e-8,
+                            method='DOP853')          
+    Qn_final_values_V.append(solution_V.y[0][-1])
+    Qn_initial_V = [solution_V.y[0][-1]]
+    #print(Vn)
+
 
 
 plt.figure(figsize=(8, 6))  # Increase figure size
@@ -78,10 +110,11 @@ plt.axhline(y=1, color='k', linestyle=':', linewidth=1)
 plt.axhline(y=-1, color='k', linestyle=':', linewidth=1)
 
 
-plt.plot(Vfe_values, Qn_final_values, label=r'$Eq.6$', color='cyan', linewidth=6)
-plt.plot(Vfe_values, Qn_final_values_II, label=r'$Eq.7$', color='coral', linestyle='-', linewidth=3)
-plt.plot(Vfe_values, Qn_final_values_III, label=r'$Eq.8$', color='springgreen', linestyle='--', linewidth=2.5)
-#plt.plot(Vfe_values, Qn_final_values_IV, label=r'$Eq.9$', color='k', linestyle='--', linewidth=1.5)
+#plt.plot(Vfe_values, Qn_final_values, label=r'df', color='cyan', linewidth=6)
+#plt.plot(Vfe_values, Qn_final_values_II, label=r'df_II', color='coral', linestyle='-', linewidth=3)
+#plt.plot(Vfe_values, Qn_final_values_III, label=r'df_III', color='springgreen', linestyle='--', linewidth=2.5)
+#plt.plot(Vfe_values, Qn_final_values_IV, label=r'df_IV', color='k', linestyle='--', linewidth=1.5)
+plt.plot(Vfe_values, Qn_final_values_V, label=r'df_V', color='green', linestyle='--', linewidth=2)
 
 plt.xlabel('$V_N$', fontsize=20)
 plt.ylabel('$Q_N$', fontsize=20)
---
+++ Python/plot_res.py
@@ -0,0 +1,112 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import re
+import numpy as np
+import matplotlib.colors as mcolors
+
+# Initialize a dictionary to hold data for each step
+data = {}
+current_step = None
+
+# Open and read the file
+file_path = 'Fe_test_res.txt'
+
+with open(file_path, 'r', encoding='ISO-8859-1') as f:
+    for line in f:
+        step_match = re.match(r'Step Information: (.*)', line)
+        if step_match:
+            current_step = step_match.group(1)
+            data[current_step] = {'time': [], 'V(in)': [], 'V(q)': []}
+        
+        # Check if the line contains data values
+        elif current_step and re.match(r'\d', line):
+            # Split the line into columns and append values to the current step's data
+            cols = line.split()
+            data[current_step]['time'].append(float(cols[0]))
+            data[current_step]['V(in)'].append(float(cols[1]))
+            data[current_step]['V(q)'].append(float(cols[2]))
+
+# List to hold step values converted to seconds and original labels
+step_values_seconds = []
+step_labels = []
+
+# Convert step values with units to seconds
+for step in data.keys():
+    vec = re.split(r'[= ]', step)
+    step_str = vec[1]  # Extract the value with unit, e.g., "1n" or "2µ"
+    print(step_str)
+    # Convert based on unit
+    if 'n' in step_str:
+        numeric_value = float(step_str.replace('n', '')) * 1e-9  # Convert nanoseconds to seconds
+    elif 'µ' in step_str or 'u' in step_str:  # Check for both "µ" and "u" for microseconds
+        numeric_value = float(step_str.replace('µ', '').replace('u', '')) * 1e-6  # Convert microseconds to seconds
+    elif 'm' in step_str:  
+        numeric_value = float(step_str.replace('m', '')) * 1e-3  # Convert microseconds to seconds
+    else:
+        numeric_value = float(step_str)  # If no unit, assume it's already in seconds
+    print(numeric_value)
+    step_values_seconds.append(numeric_value)  # Store the converted value in seconds
+    step_labels.append(step_str)  # Keep original string for labels
+
+# Set up the viridis colormap and logarithmic normalization with vmin and vmax
+vmin = min(step_values_seconds)
+vmax = max(step_values_seconds)
+norm = mcolors.LogNorm(vmin=vmin, vmax=vmax)
+viridis = plt.cm.get_cmap('winter', len(data))
+
+# Create a figure and axis
+fig, ax = plt.subplots(figsize=(10, 6))
+
+plt.axvline(x=1, color='k', linestyle=':', linewidth=1)
+plt.axvline(x=-1, color='k', linestyle=':', linewidth=1)
+plt.axhline(y=1, color='k', linestyle=':', linewidth=1)
+plt.axhline(y=-1, color='k', linestyle=':', linewidth=1)
+
+# Plot each step with a unique color based on the logarithmic color normalization
+
+ini = 590  # Assuming `ini` is the start index for slicing `values`
+
+for i, (step, values) in enumerate(list(data.items())):
+    color = viridis(norm(step_values_seconds[i]))  # Get color based on normalized log scale
+    maxq = np.max(values['V(q)'][ini:])
+    minq = np.min(values['V(q)'][ini:])
+    center = (maxq-minq)/2+minq
+    
+    ax.plot(
+        values['V(in)'][ini:], 
+        values['V(q)'][ini:]-center, 
+        linewidth=2,
+        color=color  # Use color from viridis colormap
+    )
+
+
+
+
+# Create a color bar on the right with logarithmic scale
+sm = plt.cm.ScalarMappable(cmap=viridis, norm=norm)
+sm.set_array([])  # Dummy array for ScalarMappable
+cbar = fig.colorbar(sm, ax=ax)  # Removed extend='both'
+cbar.set_label(r'$Resistance$ ($\Omega$)', labelpad=25, rotation=270, fontsize=15)
+cbar.ax.tick_params(labelsize=12) 
+# Define major ticks at key points only to keep it simple and readable
+major_ticks = [1e-3, 10e-3, 100e-3, 1]
+cbar.set_ticks(major_ticks)
+
+# Format color bar tick labels with original units
+def format_tick_label(value):
+        return f"{value:.0g}"
+
+cbar.set_ticklabels([format_tick_label(tick) for tick in major_ticks])
+
+
+plt.xlabel('$V_N$', fontsize=20)
+plt.ylabel('$Q_N$', fontsize=20)
+
+plt.xticks(fontsize=18)
+plt.yticks(fontsize=18)
+plt.minorticks_on()
+plt.tick_params(axis='both', which='major', labelsize=14, width=2, length=7)
+plt.tick_params(axis='both', which='minor', labelsize=12, width=1, length=4)
+
+
+plt.show()
