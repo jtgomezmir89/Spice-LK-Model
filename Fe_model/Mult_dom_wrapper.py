e73a7f2ea17eccdd370775e48dd9c13c64607de5
Jorge Gomez <jtgomezmir89@gmail.com>
2024-11-29 17:02:10 -0300

    MD

--- Fe_model/Mult_dom_wrapper.py
+++ Fe_model/Mult_dom_wrapper.py
@@ -1,7 +1,33 @@
 import numpy as np
 import lt_spice_model
-#Ferroelectric parameters
+import pandas as pd
+
+
+# --- Obtain Target Data --- #######################################################
+
+file_path = 'Data.xlsx'
+sheet_name = 'HZO(1-1)'
+
+# Read the sheet and skip rows to identify data
+df = pd.read_excel(file_path, sheet_name=sheet_name, skiprows=2)
+
+df.columns = df.columns.str.strip()  # Strip whitespace
+df = df.dropna(how='all', axis=1)  # Remove completely empty columns
+# centramos los loops
+
+for col in ['Vforce', 'Vforce.1', 'Vforce.2', 'Vforce.3']:
+    df[col] = df[col] - (df[col].max() + df[col].min()) / 2
+
+for col in ['Charge', 'Charge.1', 'Charge.2', 'Charge.3']:
+    df[col] = df[col] - (df[col].max() + df[col].min()) / 2
+
+
+
+# --- Ferroelectric parameters --- #################################################
 domains = 10
+Area = 2500e-8 #cm^2 
+Parasitic_resistance = 1e9
+Parasitic_capacitance = 1e-9
 
 mean_Vc = 1 
 std_dev_Vc = 0.1
@@ -9,21 +35,29 @@ std_dev_Vc = 0.1
 mean_tau = 1e-8 
 std_dev_tau = 1e-9
 
-Qo = 1
+Qo = 1e-6
 mean_Qo = Qo/domains
 std_dev_Qo = mean_Qo * 0.1
 
 Vc_values = np.random.normal(mean_Vc, std_dev_Vc, domains)
 tau_values = np.random.normal(mean_tau, std_dev_tau, domains)
 Qo_values = np.random.normal(mean_Qo, std_dev_Qo, domains)
-
 Qo_values *= Qo/np.sum(Qo_values) # Nos asegurameos que los valores sumen Qo
+lt_spice_model.generate_model(domains, Vc_values, Qo_values, tau_values, Parasitic_resistance, Parasitic_capacitance)
 
-lt_spice_model.generate_model(domains, Vc_values, Qo_values, tau_values)
-data = lt_spice_model.run_spice()
-#print(data['variables']) # Revisamos las variables que extraemos
-data_sim = lt_spice_model.analyze_result(data)
+# --- Run Sim and data anlysis --- #################################################
 
-print(data_sim)
+voltages = np.array([0.5, 1, 1.5, 2])
+data_sim={}
 
-#Generate netlist
+for v in voltages:
+    lt_spice_model.generate_sim(v)
+    data = lt_spice_model.run_spice()
+    #print(data['variables']) # Revisamos las variables que extraemos
+    data_sim = lt_spice_model.analyze_result(data, data_sim)
+
+
+
+lt_spice_model.plot_data(data_sim, df, Area)
+
+
--- Fe_model/lt_spice_model.py
+++ Fe_model/lt_spice_model.py
@@ -3,31 +3,45 @@ import paths
 from subprocess import run
 import os
 import numpy as np
+import matplotlib.pyplot as plt
 
-def generate_model(domains, Vc_values, Qo_values, tau_values ):
-
+def generate_model(domains, Vc_values, Qo_values, tau_values, Parasitic_resistance, Parasitic_capacitance):
 
     with open("MD_Model.sp", "w") as out:
         out.write("* Multi-domian SPICE model\n")
+        
+        out.write(".subckt fe_tanh_md in out\n")
 
-        out.write("V1 in 0 SINE(0 2.1 1k 0 0 0 10)")
+        for i in range(0,domains):
+            out.write("\n")
+            out.write("*Grain_{0}:".format(i))
+            out.write("\n")
+            out.write("XU_"+str(i)+" in out fe_tanh Vc="+str(Vc_values[i])+" Qo="+str(Qo_values[i])+" K=2.62 tau="+str(tau_values[i]))
+            out.write("\n")
+        out.write("Rp in out R="+str(Parasitic_resistance))
+        out.write("\n") 
+        out.write("Cp in out "+str(Parasitic_capacitance)) 
+        out.write("\n")
+        out.write(r".lib C:\Users\MBX\Desktop\Investigacion\Spice-LK-Model\Fe_model\fe_tanh.sp")
+        out.write("\n")
+        out.write(".ends")
+        out.write("\n")
+    out.close()
+
+
+def generate_sim(Voltage):
+    with open("MD_sim.sp", "w") as out:
+        out.write("* Multi-domian SPICE Simulation\n")
+        out.write("V1 in 0 SINE(0 2.1 1k 0 0 0 10)".format(Voltage))
         out.write("\n")
         out.write("Rsh 0 N0 0.00000001")  
         out.write("\n")
         out.write("B1 Q 0 V=idt(-I(Rsh))")  
         out.write("\n") 
- 
-        for i in range(0,domains):
-            out.write("\n")
-            out.write("*Grain_{0}:".format(i))
-            out.write("\n")
-            out.write("XU_"+str(i)+" in N0 fe_tanh Vc="+str(Vc_values[i])+" Qo="+str(Qo_values[i])+" K=2.62 tau="+str(tau_values[i]))
-            out.write("\n")
-
-        out.write(".tran 0 1.5m 0 1e-8")  
-        out.write("\n")    
+        out.write("XU in N0 fe_tanh_md")
+        out.write(".tran 0 1.5m 0 1e-8")     
         out.write("\n")
-        out.write(r".lib C:\Users\MBX\Desktop\Investigacion\Spice-LK-Model\Fe_model\fe_tanh.sp")
+        out.write(r".lib C:\Users\MBX\Desktop\Investigacion\Spice-LK-Model\Fe_model\fe_tanh_md.sp")
         out.write("\n")
         out.write(".backanno")    
         out.write("\n")
@@ -36,17 +50,14 @@ def generate_model(domains, Vc_values, Qo_values, tau_values ):
     out.close()
 
 
-
 def run_spice():
-    run(paths.spice_path+' -b -Run MD_Model.sp')
-    data = Py2LTSpice.read_ltspice_raw('MD_Model.raw')
+    run(paths.spice_path+' -b -Run MD_sim.sp')
+    data = Py2LTSpice.read_ltspice_raw('MD_sim.raw')
     #os.remove('MD_Model.raw')
     return data
 
 
-def analyze_result(data):
-
-    data_sim={}
+def analyze_result(data, data_sim):
     data_sim['time']={}
     data_sim['in']={}
     data_sim['q']={}
@@ -63,3 +74,51 @@ def analyze_result(data):
         elif expr =='I(Rsh)':
             data_sim['i']=np.array(data['values'][:,v])
     return data_sim
+
+def plot_data(data_sim, df, Area):
+    # Create a figure and define a grid layout for subplots
+    fig = plt.figure(figsize=(10, 6))
+    gs = fig.add_gridspec(3, 2, width_ratios=[3, 3], height_ratios=[1, 1, 1], wspace=0.3, hspace=0.5)
+
+    # Subplots on the left
+    ax1 = fig.add_subplot(gs[0, 0])  # in vs time
+    ax2 = fig.add_subplot(gs[1, 0])  # i vs time
+    ax3 = fig.add_subplot(gs[2, 0])  # q vs time
+
+    # Single subplot on the right
+    ax4 = fig.add_subplot(gs[:, 1:])  # q vs in
+
+    # Plotting the data
+    ax1.plot(data_sim['time']*1000, data_sim['in'], label="SPICE")
+    ax1.set_xlabel("Time [ms]")
+    ax1.set_ylabel("Voltage [V]")
+    ax1.legend()
+
+    ax2.plot(data_sim['time']*1000, data_sim['i'], label="SPICE", color='orange')
+    ax2.set_xlabel("Time [ms]")
+    ax2.set_ylabel("Current [A]")
+    ax2.legend()
+
+    ax3.plot(data_sim['time']*1000, data_sim['q'], label="SPICE", color='green')
+    ax3.set_xlabel("Time [ms]")
+    ax3.set_ylabel("Charge [C]")
+    ax3.legend()
+
+    #ax4.plot(data_sim['in'], data_sim['q'], label="SPICE", color='red')
+
+    ax4.plot(df['Vforce'], 1e6*df['Charge']/Area, label='0.5V')
+    ax4.plot(df['Vforce.1'], 1e6*df['Charge.1']/Area, label='1V')
+    ax4.plot(df['Vforce.2'], 1e6*df['Charge.2']/Area, label='1.5V')
+    ax4.plot(df['Vforce.3'], 1e6*df['Charge.3']/Area, label='2V')
+
+
+    ax4.legend()
+
+
+    ax4.set_xlabel("Voltage [V]")
+    ax4.set_ylabel("Polarization [$\mu C$/$cm^2$]")
+    ax4.legend()
+
+    # Show the plot
+    plt.tight_layout()
+    plt.show()
